757c8b4f5c96db547bb2ef358c4241df1abc5b7b
Arturo Camargo <kngarturo@gmail.com>
2023-12-07 17:36:11 -0500

    Subiendo
    Subiendo

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,12 @@
 import { CreateUserDto } from './dto/create-user.dto';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './entities/user.entity';
+
+import * as bcryptjs from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -14,16 +17,63 @@ export class AuthService {
     ) {}
 
 
-    create(createUserDto: CreateUserDto): Promise<User> {
-        console.log(createUserDto);        
-        const newUser = new this.userModel(createUserDto);
+    async create(createUserDto: CreateUserDto): Promise<User> {
 
-        return newUser.save();
+        try {
+            const {password, ...userData} = createUserDto;
+            const newUser = new this.userModel({
+                password: bcryptjs.hashSync(password, 10),
+                ...userData            
+            });
+
+            return await newUser.save();            
+            
+        } catch (error) {
+            if(error.code === 1100){
+                throw new BadRequestException(`${createUserDto.email} ya existe!`);            
+            }
+
+            throw new InternalServerErrorException('Error interno del servidor');
+            
+        }
+
+        //1 - Encriptar la contraseña
+
+
+        //2 - Guardar el usuario
+
+
+        //3 - Generar el JWT
+    }
+
+
+    async login(loginDto: LoginDto){
+        const {email, password} = loginDto;
+
+        const user = await this.userModel.findOne({email});
+
+        if(!user){
+            throw new UnauthorizedException('Email no valido')
+        }
+
+        if(!bcryptjs.compareSync(password, user.password)){
+            throw new UnauthorizedException('Password incorrecto')
+        }
+        
+        const {password:_, ...rest} = user.toJSON();
+
+        return {
+            user: rest,
+            token: 'ABC-123'
+        };
+        
     }
 
     findAll() {
         return `This action returns all auth`;
     }
+
+
 
     findOne(id: number) {
         return `This action returns a #${id} auth`;
