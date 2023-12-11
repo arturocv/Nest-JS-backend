95e6c9a646ef1f4c6cfc714784b36827ade1d075
Arturo Camargo <kngarturo@gmail.com>
2023-12-11 13:18:08 -0500

    Terminando servidor
    Terminando servidor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, RequestMapping } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {CreateUserDto, UpdateAuthDto, LoginDto, RegisterUserDto } from './dto/index';
+import { AuthGuard } from './guards/auth.guard';
+import { User } from './entities/user.entity';
+import { LoginResponse } from './interfaces/login-responde';
 
 @Controller('auth')
 export class AuthController {
@@ -22,23 +25,39 @@ export class AuthController {
         return this.authService.register(registerDto);
     }
 
+
+    @UseGuards(AuthGuard)
     @Get()
-    findAll() {
+    findAll(@Request() req:Request) {
+        const user = req['user'];
+        //return user;
         return this.authService.findAll();
     }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.authService.findOne(+id);
+    @UseGuards(AuthGuard)
+    @Get('/check-token')
+    ckeckToken(@Request()  req: Request): LoginResponse{
+        const user = req['user'] as User;
+
+        return {
+            user,
+            token: this.authService.getJwtToken({ id: user._id})
+        }
     }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-        return this.authService.update(+id, updateAuthDto);
-    }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.authService.remove(+id);
-    }
+    // @Get(':id')
+    // findOne(@Param('id') id: string) {
+    //     return this.authService.findOne(+id);
+    // }
+
+    // @Patch(':id')
+    // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+    //     return this.authService.update(+id, updateAuthDto);
+    // }
+
+    // @Delete(':id')
+    // remove(@Param('id') id: string) {
+    //     return this.authService.remove(+id);
+    // }
 }
--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,11 +1,48 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../interfaces/jwt.payload';
+import { AuthService } from '../auth.service';
+
+
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
+  constructor(private jwtService:JwtService,
+      private authService: AuthService
+    ){};
+
+  async canActivate(
     context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  ): Promise<boolean> {
+      const request = context.switchToHttp().getRequest();
+
+      const token = this.extractTokenFromHeader(request);
+
+      if (!token) {
+        throw new UnauthorizedException('Token no existe');
+      }      
+      
+      try {
+        const payload = await this.jwtService.verifyAsync<JwtPayload>(
+          token, { secret: process.env.JWR_SEED }
+        );  
+
+        const user = await this.authService.findUserById(payload.id);
+          if(!user) throw new UnauthorizedException('Usuario no existe');
+          if(!user.isActive) throw new UnauthorizedException('Usuario no está activo');
+
+        request['user'] = user;
+        
+      } catch (error) {
+        throw new UnauthorizedException('Token no valido')        
+      }
+
+      return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
+
